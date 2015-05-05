1e7d08b924cac8342d71e77adde30ce9410570a2
appwhattowear <app.whattowear@outlook.com>
2015-05-05 17:03:17 +0300

    https://github.com/appwhattowear/WTF.git

---
+++ tryNewCamera/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using tryNewCamera.Resources;
+
+namespace tryNewCamera
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ tryNewCamera/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using tryNewCamera.Resources;
+using Microsoft.Phone.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace tryNewCamera
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        CameraCaptureTask cameraCaptureTask;
+        private string path;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cameraCaptureTask = new CameraCaptureTask();
+            cameraCaptureTask.Show();
+            cameraCaptureTask.Completed += new EventHandler<PhotoResult>(cameraCaptureTask_Completed);
+        }
+
+        void cameraCaptureTask_Completed(object sender, PhotoResult e)
+        {
+            if (e.TaskResult == TaskResult.OK)
+            {
+                //e.ChosenPhoto.CopyTo(); 
+                path =  e.OriginalFileName;
+
+            }
+        }
+    }
+}
